53e20857d7d468d7d88bda483126c9054691c754
DineshSE13 <t.dinesh@spekond.com>
2022-11-15 16:15:59 +0530

    updated:Repo Packages

--- src/main/java/com/redis/demo/Repository/OptionRepo.java
+++ src/main/java/com/redis/demo/Repository/OptionRepo.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.redis.demo.entity.Options;
 
-public interface AnswerRepo extends JpaRepository<Options, Integer>{
+public interface OptionRepo extends JpaRepository<Options, Integer>{
 
 }
---
+++ src/main/java/com/redis/demo/entity/TestRepo.java
@@ -0,0 +1,7 @@
+package com.redis.demo.entity;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TestRepo extends JpaRepository<Test, Integer>{
+
+}
